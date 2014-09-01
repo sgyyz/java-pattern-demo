3defb85f329dd84536ca9388b2b2cbbab0983805
troyyang <troyyang@aaxischina.com>
2014-09-01 16:48:31 +0800

    create the java-pattern-demo repository.

---
+++ java-pattern-demo/src/main/java/com/troy/pattern/java_pattern_demo/App.java
@@ -0,0 +1,13 @@
+package com.troy.pattern.java_pattern_demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
