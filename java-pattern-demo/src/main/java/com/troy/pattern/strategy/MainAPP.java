4f23259ea2ba4df691834462b262f2d00e5d865d
sgyyz <yangyongzhi1989@gmail.com>
2014-09-01 17:07:35 +0800

    1. remove the original test. 2. add the test demo

---
+++ java-pattern-demo/src/main/java/com/troy/pattern/strategy/MainAPP.java
@@ -0,0 +1,7 @@
+package com.troy.pattern.strategy;
+
+public class MainAPP {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
---
+++ java-pattern-demo/src/test/java/com/troy/pattern/strategy/StrategyTest.java
@@ -0,0 +1,11 @@
+package com.troy.pattern.strategy;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class StrategyTest {
+	@Test
+	public void test() {
+		assertTrue("This value should be true.", true);
+	}
+}
